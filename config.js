eb056485cf3bee51d9f3d955e4a73f71144c4b8d
sathminafreefire2011-hub <sathminafreefire2011@gmail.com>
2026-01-27 20:42:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?J4W7oJ'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?Cs7SX7'
 };
